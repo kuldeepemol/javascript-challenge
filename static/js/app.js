a2e7ee425b4c753e535a49f72ae564111796bdd3
Kuldeep Jariwala <kuldeep@emolecules.com>
2018-09-30 01:01:35 -0700

    Added more User interaction.

--- static/js/app.js
+++ static/js/app.js
@@ -1,108 +1,118 @@
 // from data.js
 var tableData = data;
 
-var filterbtnObj = d3.select('#filter-btn');
+// function to fill the tbody element
+function fillTbody(tdData) {
 
-filterbtnObj.on('click', function() {
-    
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
-    var filteredData = tableData;
-
-    // Select the input element of id datetime and get the raw HTML node
-    var datetimeElement = d3.select("#datetime");
-    // Get the value property of the datetime element
-    var datetimeValue = datetimeElement.property("value");
-    if (datetimeValue) {
-        // filter by datetime
-        filteredData = filteredData.filter(data => data.datetime === datetimeValue );
-    }
-
-    // Select the input element of id city and get the raw HTML node
-    var cityElement = d3.select("#city");
-    // Get the value property of the city element
-    var cityValue = cityElement.property("value");
-    if (cityValue) {
-        // filter by city
-        filteredData = filteredData.filter(data => data.city === cityValue.toLowerCase());
-    }
-
-    // Select the input element of id state and get the raw HTML node
-    var stateElement = d3.select("#state");
-    // Get the value property of the state element
-    var stateValue = stateElement.property("value");
-    if (stateValue) {
-        // filter by state
-        filteredData = filteredData.filter(data => data.state == stateValue.toLowerCase());
-    }
-
-    // Select the input element of id country and get the raw HTML node
-    var countryElement = d3.select("#country");
-    // Get the value property of the country element
-    var countryValue = countryElement.property("value");
-    if (countryValue) {
-        // filter by country
-        filteredData = filteredData.filter(data => data.country === countryValue.toLowerCase());
-    }
-
-    // Select the input element of id shape and get the raw HTML node
-    var shapeElement = d3.select("#shape");
-    // Get the value property of the datetime element
-    var shapeValue = shapeElement.property("value");
-    if (shapeValue) {
-        // filter by shape
-        filteredData = filteredData.filter(data => data.shape === shapeValue.toLowerCase());
-    }
+    // Use D3 to select the table body
+    var resultCountObj = d3.select("#results-count");
+    resultCountObj.text('Total Rows: ' + tdData.length);
 
     // Use D3 to select the table body
     var tbody = d3.select("tbody");
     tbody.text('');
-  
+
     // Iterate through each filteredData object and 
-    filteredData.forEach(data => {
+    tdData.forEach(data => {
 
         // Append one table row per data
         var row = tbody.append("tr");
-    
+
         // append one cell for the each key/value of the object
-        row.append("td").text(data.datetime);
-        row.append("td").text(data.city);
-        row.append("td").text(data.state);
-        row.append("td").text(data.country);
-        row.append("td").text(data.shape);
-        row.append("td").text(data.durationMinutes);
-        row.append("td").text(data.comments);
-    
+        Object.values(data).forEach(item => {
+            row.append("td").text(item);
+        });
     });
+}
 
-});
-
-var showAllbtnObj = d3.select('#show-all-btn');
-
-showAllbtnObj.on('click', function() {
-    
+// Function to handle event change on form
+function handleChangeEvent() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
     var filteredData = tableData;
 
-    // Use D3 to select the table body
-    var tbody = d3.select("tbody");
-    tbody.text('');
-  
-    // Iterate through each filteredData object and 
-    filteredData.forEach(data => {
-
-        // Append one table row per data
-        var row = tbody.append("tr");
+    // Get all Input elements
+    var allInput = d3.selectAll('input');
+    allInput.nodes().forEach(eachInput => {
+        var inputId = eachInput.id;
+        var inputValue = eachInput.value;
+        var re = new RegExp('^' + inputValue.toLowerCase());
+        
+        if (inputId == 'datetime' && inputValue) {
+            // filter by datetime
+            filteredData = filteredData.filter(data => data.datetime.match(re));
+        }
     
-        // append one cell for the each key/value of the object
-        row.append("td").text(data.datetime);
-        row.append("td").text(data.city);
-        row.append("td").text(data.state);
-        row.append("td").text(data.country);
-        row.append("td").text(data.shape);
-        row.append("td").text(data.durationMinutes);
-        row.append("td").text(data.comments);
+        if (inputId == 'city' && inputValue) {
+            // filter by datetime
+            filteredData = filteredData.filter(data => data.city.match(re));
+        }
     
+        if (inputId == 'state' && inputValue) {
+            // filter by state
+            filteredData = filteredData.filter(data => data.state.match(re));
+        }
     });
+    
+    // Get all select elements
+    var allSelect = d3.selectAll('select');
+    allSelect.nodes().forEach(eachSelect => {
+        var selectId = eachSelect.id;
+        var selectValue = eachSelect.value;
+        if (selectId == 'country-dd' && selectValue) {
+            // filter by country
+            filteredData = filteredData.filter(data => data.country == selectValue);
+        }
+    
+        if (selectId == 'shape-dd' && selectValue) {
+            // filter by shape
+            filteredData = filteredData.filter(data => data.shape == selectValue);
+        }
+    });
+
+    fillTbody(filteredData);
+}
+
+// function to clear all input value
+function clearAllInput() {
+    var allInput = d3.selectAll('input');
+    allInput.nodes().forEach(eachInput => {
+        eachInput.value = '';
+    });
+
+    var allSelect = d3.selectAll('select');
+    allSelect.nodes().forEach(eachSelect => {
+        eachSelect.value = '';
+    });
+
+}
+
+// form event listener
+var form = d3.select('form');
+form.on('change keyup', handleChangeEvent);
+
+var clearAllBtnObj = d3.select('#clear-all-btn');
+clearAllBtnObj.on('click', function() {
+    
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    clearAllInput();
+    fillTbody(tableData);
 });
+
+// Dynamic unique Countries in the drop-down
+var selectCountryObj = d3.select('#country-dd');
+var uniqueCountries = Array.from(new Set(tableData.map(d => d.country)));
+uniqueCountries.forEach(countryValue => {
+    selectCountryObj.append('option').attr('value', countryValue).text(countryValue);
+});
+
+// Dynamic Shape in the select drop-down for shapes
+var selectShapeObj = d3.select('#shape-dd');
+var uniqueShapes = Array.from(new Set(tableData.map(d => d.shape)));
+uniqueShapes.forEach(shapeValue => {
+    selectShapeObj.append('option').attr('value', shapeValue).text(shapeValue);
+});
+
+// initail call to fillTbody with all the data.
+fillTbody(tableData);
